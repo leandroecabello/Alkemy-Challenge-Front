926dfbccb3c2782ed17dcb421c89c22c6c4291dd
leandro <leandro.ecabello@gmail.com>
2020-12-21 02:32:49 -0300

    Modify ListPost component

--- src/components/ListPost/ListPost.jsx
+++ src/components/ListPost/ListPost.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import {
+  Col, Container, ListGroup, Row
+} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './ListPost.css'
 
 const ListPost = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.data
+    setPosts(data)
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  return (
+    <Container className="my-4">
+      <Row>
+        <Col className="my-4">
+          <ListGroup as="ul">
+            {
+              posts.map((post) => {
+                return (
+                  <ListGroup.Item as="li" key={post.id}>
+                    <Link className="linkTitle" to="/">{post.title}</Link>
+                  </ListGroup.Item>
+                )
+              })
+            }
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ListPost
