3bb6723eea413adea8621ac250fb4f690ca05a43
leandro <leandro.ecabello@gmail.com>
2020-12-21 02:29:20 -0300

    Create Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+const Navigation = () => {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Challenge-Front</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/createPost">Create Post</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation
