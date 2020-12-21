ecad6940c9321db70105bf3747fbb76f0506e12f
leandro <leandro.ecabello@gmail.com>
2020-12-21 20:13:18 -0300

    Modify Navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
+import './Navigation.css'
 
 const Navigation = () => {
   return (
     <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Challenge-Front</Navbar.Brand>
+        <Navbar.Brand href="/">Challenge-Front</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav className="ml-auto text-center">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/createPost">Create Post</Nav.Link>
           </Nav>
